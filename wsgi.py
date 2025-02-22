b85f553ba2ff82beccbf3a874c41f7266ed62186
Şafak şenal <63151280+SaFaKsenall@users.noreply.github.com>
2025-02-23 00:51:03 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
-from bot import app
+from flask import Flask
+from bot import app  # Telegram botu için gerekli olan app'i buradan alıyoruz
+
+# Flask uygulamasını oluştur
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8000)  # Flask uygulamasını başlat
